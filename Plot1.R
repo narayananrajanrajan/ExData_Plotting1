9733656c93d8f084e96286df3b7979f7762d485d
narayananrajan <narayananrajan@gmail.com>
2017-06-19 13:14:51 -0700

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,25 @@
+#######################Build  histogram###########################
+
+
+#############################
+#Read the file###############
+#############################
+data <- read.table(file="household_power_consumption.txt", sep=";", header=TRUE)
+
+#head(data,1)
+#str(data)
+#data1 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#############################
+#Retrieve only numeric data##
+#############################
+globalactivepower <- as.numeric(data$Global_active_power)
+
+#head (globalactivepower)
+
+#############################
+#Plot the Histogram##########
+#############################
+png("plot1.png",width=480,height=480)
+hist(globalactivepower,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
